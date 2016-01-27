31c2a18e70340fec23d5643e674e08e90682928a
Reductions <slavi.boyanov@gmail.com>
2016-01-27 08:45:52 +0200

    Add and commit reworked

--- solutions/05.rb
+++ solutions/05.rb
@@ -19,6 +19,25 @@ class ObjectStore
     end
   end
 
+  class Commit
+    attr_reader :content, :message, :date, :hash
+
+    def initialize(content = {}, message = "")
+      @content = content
+      @message = message
+      @date = Time.now
+      @hash = Digest::SHA1.hexdigest(@date.to_s + message )
+    end
+
+    def objects
+      @content.values
+    end
+
+    def to_s
+      "Commit #{@hash}\nDate: #{@date}\n\n\t#{@message}"
+    end
+  end
+
   class BranchManager
     class Branch
       attr_accessor :added, :removed, :commited
@@ -28,33 +47,38 @@ class ObjectStore
         @removed = removed
         @commited = commited
       end
+
+      private
+
+      def commit(message)
+        count = @added.size + @removed.size
+        content = @commited[-1].content.merge(@added).
+                  reject { |key, _| @removed.include?(key) }
+        @commited << Commit.new(content, message)
+        Message.new("#{message}\n\t#{count} objects changed")
+      end
     end
+
+    attr_accessor :name, :current
 
     def initialize
       @branches = {master: Branch.new([Commit.new])}
-      @current = :master
-    end
-
-    def name
-      @current.to_s
-    end
-
-    def current
-      @branches[@current]
+      @name = :master
+      @current = @branches[:master]
     end
 
     def create(name)
-      if @branches.has_key(name.to_sym)
+      if @branches.has_key(symbol_name = name.to_sym)
         Message.new("Branch #{name} already exists.", false)
       else
-        @branches[name.to_sym] = @branches[@current]
+        @branches[symbol_name] = @current
         Message.new("Creat branch #{name}.")
       end
     end
 
     def checkout(branch_name)
-      if @branches.has_key?(branch_name.to_sym)
-        @current = branch_name.to_sym
+      if @branches.has_key?(symbol_name = branch_name.to_sym)
+        @current, @name = @branches[symbol_name], symbol_name
         Message.new("Switched to branch #{@current}")
       else
         Message.new("Branch #{branch_name} does not exist", false)
@@ -62,7 +86,7 @@ class ObjectStore
     end
 
     def remove(branch_name)
-      if branch_name.to_sym == @current
+      if branch_name.to_sym == @name
         Message.new("Can't remove current branch.", false)
       elsif @branches.has_key(branch_name.to_sym)
         Message.new("Branch #{branch_name} does not exist.", false)
@@ -72,137 +96,95 @@ class ObjectStore
     end
 
     def list
-      message = @branches.keys.sort { |first, second| first <=> second }
-                .map do |item|
-        if item == @current
+      message = @branches.keys.sort.map do |item|
+        if item == @name
           "* #{item}"
         else
           item.to_s
         end
-      end
-                .join("\n")
+      end.join("\n")
       Message.new(message)
     end
   end
 
-  class ContentManager
-    class Commit
-      attr_reader :content, :message, :date, :hash
+  attr_reader :branch
 
-      def initialize(content = {}, message = "")
-        @content = content
-        @message = message
-        @date = Time.now
-        @hash = Digest::SHA1.hexdigest(@date.to_s + message )
-      end
+  def initialize
+    @branch = BranchManager.new
+  end
 
-      def objects
-        @content.values
-      end
+  def add(name, object)
+    added[name.to_sym] = object
+    Message.new("Added #{name} to stage.", true, object)
+  end
 
-      def to_s
-        "Commit #{@hash}\nDate: #{@date}\n\n\t#{@message}"
-      end
+  def commit(message)
+    if added.empty? and removed.empty?
+      Message("Nothing to commit, working directory clean.", false)
+    else
+      @branch.current.send(:commit, message)
     end
+  end
 
-    attr_accessor :branch
+  def remove(name)
+  end
 
-    def initialize
-      @branch = BranchManager.new
+  def checkout(hash)
+  end
+
+  def head
+    if commited.size > 1
+      Message.new("#{commited[-1].message}", true, commited[-1])
+    else
+      Message.new("Branch #{@branch.name} does not have any commits yet.",
+                  false)
     end
+  end
 
-    def add(name, object)
-      added[name.to_sym] = object
-      Message.new("Added #{name} to stage.", true, object)
+  def log
+    if commited.size == 1
+      Message.new("Branch #{@branch.name} does not have any commits yet.",
+                  false)
+    else
+      message = commited[1..-1].map { |item| item.to_s }.join("\n\n")
+      Message.new(message)
     end
+  end
 
-    def commit(message)
-      if added.empty?
-        Message.new("Nothing to commit, working directory clean.", false)
-      else
-        count = added.size + removed.size
-        new_content = commited[-1].content.merge(added)
-                      .delete_if { |key, _| removed.member?(key) }
-        commited.push(Commit.new(new_content,message))
-        added, removed = {}, []
-        Message.new("#{message}\n\t#{count} objects changed",
-                        true,
-                        commited[-1])
-      end
-    end
-
-    def remove(name)
-      if commited[-1].content.hash_key(name.to_sym)
-        removed.push(name.to_sym)
-        Message.new("Added #{name} for removal.",
-                        true,
-                        @commited[-1].content[name.to_sym])
-      else
-        Message.new("Object #{name} is not committed.", false)
-      end
-    end
-
-    def checkout(hash)
-      index = commited.find_index { |item| item.hash == hash }
-      if index
-        @branch.current.commited = commited[0..index]
-        Message.new("HEAD is now at #{hash}", true, commited[-1])
-      else
-        Message.new("Commit #{hash} does not exist.", false)
-      end
-    end
-
-    def head
-      if commited.size > 1
-        Message.new("#{commited[-1].message}", true, commited[-1])
-      else
-        Message.new("Branch #{@branch.name} does not have any commits yet.",
-                        false)
-      end
-    end
-
-    def log
-      if commited.size == 1
-        Message.new("Branch #{@branch.name} does not have any commits yet.",
-                        false)
-      else
-        message = commited[1..-1].map { |item| item.to_s }.join("\n\n")
-        Message.new(message)
-      end
-    end
-
-    def get(name)
-      if commited[-1].content.has_key?(name.to_sym)
-        Message.new("Found object #{name}.",
-                        true,
-                        commited[-1].content[name.to_sym])
-      else
-        Message.new("Object #{name} is not commited.", false)
-      end
-    end
-
-    def execute
-      yield
-    end
-
-    private
-
-    def added
-      @branch.current.added
-    end
-
-    def commited
-      @branch.current.commited
-    end
-
-    def removed
-      @branch.current.removed
+  def get(name)
+    if commited[-1].content.has_key?(name.to_sym)
+      Message.new("Found object #{name}.",
+                  true,
+                  commited[-1].content[name.to_sym])
+    else
+      Message.new("Object #{name} is not commited.", false)
     end
   end
 
   def self.init(&block)
-    container = ContentManager.new
+    container = self.new
     container.instance_eval(&block) if block_given?
     container
   end
+
+  private
+
+  def added
+    @branch.current.added
+  end
+
+  def commited
+    @branch.current.commited
+  end
+
+  def removed
+    @branch.current.removed
+  end
 end
+
+repo = ObjectStore.init
+repo.add("asd", 1)
+repo.add("sdw", 2)
+repo.commit("first")
+h = repo.head.result
+p h
